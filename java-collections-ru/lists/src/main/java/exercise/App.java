57ed8399d4bf44b0b7d58509259165ca557a7313
@hexlet/cli <support@hexlet.io>
2023-07-10 20:18:17 +0300

    submit java-collections-ru/lists

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,31 @@
+package exercise;
+
+import java.util.*;
+
+// BEGIN
+class App {
+    public static boolean scrabble (String str1, String str2){
+
+        List<Character> list1 = new LinkedList<>();
+        for (int x = 0; x < str1.length(); x++) {
+            list1.add(str1.toLowerCase().charAt(x));
+        }
+
+        List<Character> list2 = new LinkedList<>();
+        for (int x = 0; x < str2.length(); x++) {
+            list2.add(str2.toLowerCase().charAt(x));
+        }
+
+        ArrayList <Character> finalResult = new ArrayList <Character> ();
+
+        for (int i = 0; i < list1.size(); i++){
+            if (list2.contains(list1.get(i))){
+                list2.remove(list1.get(i));
+            }
+        }
+
+        return list2.isEmpty();
+    }
+}
+
+//END
