85e7e767d9bbcc33110db90da4b1b2a5946bf9f9
@hexlet/cli <support@hexlet.io>
2023-10-14 18:17:38 +0300

    submit java-collections-ru/other-collections

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -7,10 +7,27 @@ import java.util.TreeSet;
 
 // BEGIN
 class App {
-    public static Map genDiff(Map<String, Object> map1,Map<String,Object> map2) {
-        Map<String, String> expected22 = new LinkedHashMap<>();
+    public static Map<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
 
-        return expected22;
+        Map<String, String> result = new LinkedHashMap<>();
+        Set<String> keys = new TreeSet<>(data1.keySet());
+        keys.addAll(data2.keySet());
+
+        for (String key: keys) {
+
+            if (!data1.containsKey(key)) {
+                result.put(key, "added");
+            } else if (!data2.containsKey(key)) {
+                result.put(key, "deleted");
+            } else if (data1.get(key).equals(data2.get(key))) {
+                result.put(key, "unchanged");
+            } else {
+                result.put(key, "changed");
+            }
+        }
+
+        return result;
     }
 }
+
 //END
