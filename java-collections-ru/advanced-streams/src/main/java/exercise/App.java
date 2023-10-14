febd5a8ff4fe4a65a2f0518c9272e3a838be920c
@hexlet/cli <support@hexlet.io>
2023-10-14 16:19:00 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -10,38 +10,17 @@ import java.util.stream.Stream;
 // BEGIN
 class App {
     public static String getForwardedVariables(String str) {
-        
-        str = str.replaceAll(",","");
-        str = str.replaceAll(" ","");
 
-        List<String> list = new ArrayList<String>(Arrays.asList(str.split("\"")));
-        String str2 = list.stream()
-                .filter(x -> x.contains("="))
-                .filter(x -> x.contains("X_FORWARDED_"))
-                .collect(Collectors.toList()).toString();
-        List<String> list2 = new ArrayList<String>(Arrays.asList(str2.split("X_FORWARDED_")));
-        String list3 = list2.stream()
-                .filter(x -> x.contains("="))
-                .filter(x -> !x.contains("key="))
-                .map(Object::toString)
+        String[] lines = str.split("\n");
+        return Arrays.stream(lines)
+                .filter(line -> line.startsWith("environment="))
+                .map(line -> line.replaceAll("environment=", ""))
+                .map(line -> line.replaceAll("\"", ""))
+                .map(line -> line.split(","))
+                .flatMap(Arrays::stream)
+                .filter(kv -> kv.startsWith("X_FORWARDED_"))
+                .map(kv -> kv.replaceFirst("X_FORWARDED_", ""))
                 .collect(Collectors.joining(","));
-        if (str.contains("variable=value")) {
-            return list3.replaceAll("]","");
-        } else {
-            return "var1=111,var2=123,var3=value,mail=tirion@google.com,HOME=/home/tirion";
-        }
-
-
-//        System.out.println(list2);
-//        return list2.get(0);
-//        rightStr.remove(0);
-//        return rightStr.stream()
-//                .filter(x -> !x.contains("key"))
-//                .collect(Collectors.toList())
-//                .toString();
-//                .map(Object::toString)
-//                .collect(Collectors.joining(","));
-//        return rightStr.toString();
     }
 }
 //END
