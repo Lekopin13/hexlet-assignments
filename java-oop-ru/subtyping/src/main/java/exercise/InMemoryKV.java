af523da5c8842847d03a750ddb0ee1810c522433
@hexlet/cli <support@hexlet.io>
2023-10-28 15:28:03 +0300

    submit java-oop-ru/subtyping

--- java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
+++ java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
@@ -5,30 +5,31 @@ import java.util.HashMap;
 
 // BEGIN
 public class InMemoryKV implements KeyValueStorage {
-    private Map<String, String> map = new HashMap<>();
+    private Map<String, String> data = new HashMap<>();
+    InMemoryKV(Map<String, String> initial) {
+        data.putAll(initial);
+    }
 
 
 
     @Override
     public void set(String key, String value) {
-
+    data.put(key, value);
     }
 
     @Override
     public void unset(String key) {
-
+    data.remove(key);
     }
 
     @Override
     public String get(String key, String defaultValue) {
-        map.get(key);
-        return null;
+        return data.getOrDefault(key, defaultValue);
     }
 
     @Override
     public Map<String, String> toMap() {
-
-        return null;
+        return new HashMap<>(data);
     }
 
 
