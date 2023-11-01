2a62b19cf763270cbbb00c272e0bebf1e75587e3
@hexlet/cli <support@hexlet.io>
2023-11-01 22:14:09 +0300

    submit java-oop-ru/inheritance

--- java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
+++ java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
@@ -5,5 +5,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 // BEGIN
-
+class PairedTag {
+    private Map<String, String> singleTag;
+    private String k;
+    private String v;
+    public String doAttributes(Tag tag, Map singleTag) {
+        return "<" + tag + " class=" + singleTag + " id=" + singleTag + ">";
+    }
+}
 // END
